2945599b6b46e90058f1a545f950b6053f041d7d
Akagami Rex <abhayrathour128125@gmail.com>
2024-09-28 18:11:31 +0530

    KeyControlFix

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -12,22 +12,22 @@ public class InputHandler extends KeyAdapter {
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 
-		if(key == KeyEvent.VK_LEFT && !snake.isLeft()){
+		if(key == KeyEvent.VK_LEFT && !snake.isLeft() && !snake.isRight()){
 			snake.setDown(false);
 			snake.setUp(false);
 			snake.setLeft(true);
 		}
-		if(key == KeyEvent.VK_RIGHT && !snake.isRight()){
+		if(key == KeyEvent.VK_RIGHT && !snake.isRight() && !snake.isLeft()){
 			snake.setDown(false);
 			snake.setUp(false);
 			snake.setRight(true);
 		}
-		if(key == KeyEvent.VK_UP && !snake.isUp()){
+		if(key == KeyEvent.VK_UP && !snake.isUp() && !snake.isDown()){
 			snake.setLeft(false);
 			snake.setUp(true);
 			snake.setRight(false);
 		}
-		if(key == KeyEvent.VK_DOWN && !snake.isDown()){
+		if(key == KeyEvent.VK_DOWN && !snake.isDown() && !snake.isUp()){
 			snake.setLeft(false);
 			snake.setRight(false);
 			snake.setDown(true);
